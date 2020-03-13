77dc290a43a9a9b126181776b41688331641ee84
Hamzawy63 <hamzahasssan835@gmail.com>
2020-03-13 20:24:13 +0200

    implemented build method for max heap

--- src/eg/edu/alexu/csd/filestructure/sort/MaxHeap.java
+++ src/eg/edu/alexu/csd/filestructure/sort/MaxHeap.java
@@ -1,12 +1,19 @@
 package eg.edu.alexu.csd.filestructure.sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class MaxHeap<T extends Comparable<T>> implements IHeap<T> {
     INode[] elements = null;
-    int MAX = 1000000;
+    int MAX = 1000005;
     int heapSize = 0;
+
+    public INode get(int index) {
+        if (index > heapSize) return null;
+        return elements[index];
+    }
 
     MaxHeap() {
         elements = new INode[MAX];
@@ -65,23 +72,46 @@ public class MaxHeap<T extends Comparable<T>> implements IHeap<T> {
         heapSize++;
 
     }
+    private void buildMaxHeap(){
+        for(int i = heapSize/2 ; i >= 1 ; i--) {
+            heapify(elements[i]);
+        }
+    }
 
     @Override
     public void build(Collection<T> unordered) {
-
+        heapSize = unordered.size() ;
+        int index = 1;
+        for (Iterator<T> iterator = unordered.iterator(); iterator.hasNext(); ) {
+            elements[index] = new Node(this, index, iterator.next());
+            index++;
+        }
+        buildMaxHeap() ;
     }
 
     /*Helping methods */
 
     private INode<T> lastElement() {
-
         if (empty())
             return null;
-        return elements[heapSize + 1];
+        return elements[heapSize];
     }
 
     private boolean empty() {
         return heapSize == 0;
     }
+    private void print() {
+        for(int i = 1 ; i< heapSize ; i++ ) {
+            System.out.println(elements[i].toString()) ;
+        }
+    }
+
+
+//    public static void main(String[] args) {
+//        MaxHeap<Integer> heaph5a = new MaxHeap<>() ;
+//        ArrayList<Integer> sample = new ArrayList<>(Arrays.asList(1,2,3,4,5,4,4,5,5,2,4,5,1,2,5,4,7,8,9)) ;
+//        heaph5a.build(sample) ;
+//       heaph5a.print();
+//    }
 
 }
--- src/eg/edu/alexu/csd/filestructure/sort/Node.java
+++ src/eg/edu/alexu/csd/filestructure/sort/Node.java
@@ -1,23 +1,29 @@
 package eg.edu.alexu.csd.filestructure.sort;
 
 public class Node<E extends Comparable<E>> implements INode<E> {
-    private MaxHeap<E> heap ;
-    private E val = null ;
-    private int index  ;
+    private MaxHeap<E> heap;
+    private E val = null;
+    private int index;
+
+    public Node(MaxHeap<E> maxHeap, int index , E val ) {
+        heap = maxHeap;
+        this.index = index;
+        this.val = val ;
+    }
+
     @Override
-    public INode<E> getLeftChild()
-    {
-        return null;
+    public INode<E> getLeftChild() {
+        return heap.get(2 * index);
     }
 
     @Override
     public INode<E> getRightChild() {
-        return null;
+        return heap.get(2 * index + 1);
     }
 
     @Override
     public INode<E> getParent() {
-        return null;
+        return heap.get(index / 2);
     }
 
     @Override
@@ -27,6 +33,11 @@ public class Node<E extends Comparable<E>> implements INode<E> {
 
     @Override
     public void setValue(E value) {
-        val = value ;
+        this.val = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.val.toString() ;
     }
 }
